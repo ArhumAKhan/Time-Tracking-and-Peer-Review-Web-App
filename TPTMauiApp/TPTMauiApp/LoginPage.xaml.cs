fd5b29d3dc96a994640747897c5db7c2ee04ed98
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-10-21 15:23:28 -0500

    Update LoginPage.xaml.cs

--- TPTMauiApp/TPTMauiApp/LoginPage.xaml.cs
+++ TPTMauiApp/TPTMauiApp/LoginPage.xaml.cs
@@ -1,4 +1,5 @@
 using System.Text.RegularExpressions;
+using MySql.Data.MySqlClient;
 
 namespace TPTMauiApp
 {
@@ -21,13 +22,51 @@ namespace TPTMauiApp
             }
 
             string pattern = @"^[A-Za-z]{3}[0-9]{6}$";
+
             if (!Regex.IsMatch(netID, pattern))
             {
                 await DisplayAlert("Error", "Invalid NetID.", "OK");
                 return;
             }else
             {
-                await Navigation.PushAsync(new MainPage());
+                Authenticate(netID, password);
+            }
+        }
+
+        private async void Authenticate(string netID, string password)
+        {
+            string connectionString = "server=localhost;user=root;database=team73_db;port=3306;password=team73";
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    await conn.OpenAsync();
+
+                    string query = "SELECT utd_id FROM users WHERE net_id = @netID AND password = @password AND user_type = @userType";
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@netID", netID);
+                        cmd.Parameters.AddWithValue("@password", password);
+                        cmd.Parameters.AddWithValue("@userType", 'S');
+
+                        var result = Convert.ToInt32(await cmd.ExecuteScalarAsync());
+
+                        if (result > 0)
+                        {
+                            await Navigation.PushAsync(new MainPage());
+                        }
+                        else
+                        {
+                            await DisplayAlert("Error", "Invalid NetID or Password.", "OK");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", $"An error occurred: {ex.Message}", "OK");
             }
         }
     }
