0b95c2446f973500ecec7018cc5078a65547c527
Farhat R. Kabir <148591416+farhatraiyan@users.noreply.github.com>
2024-11-30 06:25:42 -0600

    PR Builder Page Documentation

--- MauiApp/Tracker/PRBuilder.xaml.cs
+++ MauiApp/Tracker/PRBuilder.xaml.cs
@@ -3,13 +3,30 @@ using MySql.Data.MySqlClient;
 
 namespace Tracker
 {
+    // ******************************************************************************
+    // * Peer Review Builder Page for Tracker Application
+    // *
+    // * Written by Farhat R. Kabir for CS 4485.
+    // * NetID: frk200000
+    // *
+    // * This page allows professors to build Peer Review Forms for their classes.
+    // * The page allows the user to enter a Peer Review Name, Start Date, End Date, and
+    // * a list of criteria for the Peer Review. Upon submission, the Peer Review is created.
+    // *
+    // ******************************************************************************
+
     public partial class PRBuilder : ContentPage
     {
+        // ** Constructor **
+        // Initializes the login page and its components.
         public PRBuilder()
         {
             InitializeComponent();
         }
 
+        // ** Add Criterion Clicked Event **
+        // This method is called when the user clicks the Add Criterion button. It adds a new
+        // criterion entry fields.
         private void AddCriterion_Clicked(object sender, EventArgs e)
         {
             // Create a new Label and Entry for the criteria
@@ -17,8 +34,12 @@ namespace Tracker
             CriteriaStack.Children.Add(new Entry { WidthRequest = 420, Margin = new Thickness(0, 0, 0, 10), Placeholder = "E.G. Attendance, Participation, etc.", TextColor = Colors.Black });
         }
 
+        // ** Remove Criterion Clicked Event **
+        // This method is called when the user clicks the Remove Criterion button. It removes
+        // the last criterion entry field.
         private void RemoveCriterion_Clicked(object sender, EventArgs e)
         {
+            // Only remove if there is more than one criterion entry (with its label)
             if (CriteriaStack.Children.Count > 2)
             {
                 CriteriaStack.Children.RemoveAt(CriteriaStack.Children.Count - 1);
@@ -26,6 +47,10 @@ namespace Tracker
             }
         }
 
+        // ** Submit Clicked Event **
+        // Form submission method for creating a Peer Review. It retrieves data inputted
+        // by the user, creates a new Peer Review in the database, and adds the criteria
+        // to the Peer Review.
         private void Submit_Clicked(object sender, EventArgs e)
         {
             string startDate = StartDateEntry.Date.ToString("yyyy-MM-dd");
@@ -46,6 +71,7 @@ namespace Tracker
 
             connection.Open();
 
+            // Insert Peer Review into the database
             string pr_insert = "INSERT INTO peer_review (start_date, end_date) VALUES (@startDate, @endDate);";
 
             using var command = new MySqlCommand(pr_insert, connection);
@@ -58,6 +84,7 @@ namespace Tracker
             int pr_id = (int)command.LastInsertedId;
             System.Diagnostics.Debug.WriteLine("Last Inserted ID: " + pr_id);
 
+            // Add each criterion to the Peer Review
             foreach (string criteria in criteria_list)
             {
                 string criteria_insert = "INSERT INTO pr_criteria (pr_id, criteria_desc) VALUES (@pr_id, @criteria);";
@@ -72,14 +99,17 @@ namespace Tracker
 
             connection.Close();
 
+            // Display success message
             DisplayAlert("Success", "Peer Review Created Successfully", "OK");
 
+            // Clear the form fields
             PRNameEntry.Text = string.Empty;
             StartDateEntry.Date = DateTime.Now;
             EndDateEntry.Date = DateTime.Now;
             CriteriaEntry.Text = string.Empty;
-
             CriteriaStack.Children.Clear();
+
+            // Add a new criterion entry field
             CriteriaStack.Children.Add(new Label { Text = "Enter Criteria" });
             CriteriaStack.Children.Add(new Entry { WidthRequest = 420, Margin = new Thickness(0, 0, 0, 10), Placeholder = "E.G. Attendance, Participation, etc.", TextColor = Colors.Black });
         }
